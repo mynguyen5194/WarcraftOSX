4f303013c872f3d8f53b510b55f242e08dd522b8
Michelle Lee <smile2mlee@gmail.com>
2017-02-06 13:30:24 -0800

    updated GraphicFactory

--- WarcraftOSX/GraphicFactory.swift
+++ WarcraftOSX/GraphicFactory.swift
@@ -32,12 +32,23 @@ class GraphicFactory: NSView {
 //        layer.context!.draw(image.cgImage!, in: CGRect(origin: .zero, size: image.size))
 //        UIGraphicsEndImageContext()
         
+        //terrain png to CGImage
+        let terrainURL = URL(fileURLWithPath: (Bundle.main.path(forResource: "data/img/Terrain", ofType:"png"))!)
+        let terrainData = CGDataProvider(url: terrainURL as CFURL)
+        let terrainCG = CGImage(pngDataProviderSource: terrainData!, decode: nil, shouldInterpolate: false, intent: CGColorRenderingIntent.defaultIntent)
+        let terrainSize = CGSize(width: terrainCG!.width, height: terrainCG!.height)
+        
+        
         let mapContext = NSGraphicsContext.current()?.cgContext
         
         //create initial layer
         let mapSize = CGSize(width: 32, height: 32)
-        let mapLayer = CGLayer(mapContext!, size: mapSize, auxiliaryInfo: nil)
+        let mapLayer = CGLayer(mapContext!, size: terrainSize, auxiliaryInfo: nil)
         var mapLayerContext = mapLayer?.context
+        let mapOrigin = CGPoint(x: 0, y: 0)
+        let mapRect = CGRect(origin: mapOrigin, size: mapSize)
+        mapLayerContext?.draw(terrainCG!, in: mapRect)
+        //mapLayerContext?.draw(terrainCG!, in: CGRect(origin: mapOrigin, size: terrainSize))
         
         return mapLayer!
         fatalError("This method is not yet implemented.")
