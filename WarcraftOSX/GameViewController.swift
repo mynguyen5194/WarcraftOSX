76a14f2cdb23bd09bae013a8308c27d997b26545
kmsolomo <kmsolomon@ucdavis.edu>
2017-02-07 12:21:10 -0800

    restructure view controller

--- WarcraftOSX/GameViewController.swift
+++ WarcraftOSX/GameViewController.swift
@@ -13,11 +13,31 @@ import SpriteKit
 
 class GameViewController: NSViewController {
 
+    @IBOutlet var mainGameView: NSView!
+    @IBOutlet weak var gameSceneView: SKView!
+    @IBOutlet weak var gameSideBarView: NSView!
+    @IBOutlet weak var resourceBarView: NSView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let scene = GameScene(fileNamed: "GameScene") {
+            // Configure the view.
+            let skView: SKView = gameSceneView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .fill
+            
+            skView.presentScene(scene)
+        }
+        self.view = mainGameView
+
+        /*
         do{
-            /*
+            
             //Loading Rendering Configuration about line 406 in ApplicationData.cpp
             let mapRenderingURL = URL(fileURLWithPath: (Bundle.main.path(forResource: "data/img/MapRendering", ofType: "dat"))!)
             let mapDataSource = try FileDataSource(url: mapRenderingURL)
@@ -35,12 +55,11 @@ class GameViewController: NSViewController {
             try map2PlayerAssetDec.loadMap(source: map2PlayerDataSource)
             
             let map = try MapRenderer(configuration: mapDataSource, tileset: terrainTileset, map: map2PlayerAssetDec)
-            */
-            if let scene = GameScene()
+            
             
         } catch{
             print(error)
-        }
+        }*/
 
         
     }
