a9e957932def2bfb33899781fd855a771c87ef7f
kmsolomo <kmsolomon@ucdavis.edu>
2017-02-09 11:17:47 -0800

    minor tweaks

--- WarcraftOSX/GraphicFactory.swift
+++ WarcraftOSX/GraphicFactory.swift
@@ -2,7 +2,7 @@ import Foundation
 import CoreGraphics
 import AppKit
 
-class GraphicFactory {
+class GraphicFactory: NSObject {
     static func createSurface(width: Int, height: Int, format: GraphicSurfaceFormat) -> GraphicSurface? {
         let size = CGSize(width: width, height: height)
         let colorSpace:CGColorSpace = CGColorSpaceCreateDeviceRGB()
@@ -14,26 +14,23 @@ class GraphicFactory {
         return layer
     }
     
+    static func loadSurface(dataSource: DataSource) -> GraphicSurface? {
+        fatalError("This method is not yet implemented.")
+    }
+    
     static func loadPNGTilesetSurface(name: String) -> GraphicSurface {
         let name = String(name.characters.dropFirst(2))
         let image = NSImage.init(named: name)
         let originPoint = CGPoint.init(x: 0, y: 0)
-        
-        let imageRect:CGRect = CGRect.init(origin: originPoint, size: image!.size)
-        //let imageRef = image?.cgImage(forProposedRect: imageRect, context: nil, hints: nil) to convert to cgimage
-        
+        var imageRect:CGRect = CGRect.init(origin: originPoint, size: image!.size)
+        let imageRef = image?.cgImage(forProposedRect: &imageRect, context: nil, hints: nil)
         let colorSpace:CGColorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-        
-        var width: Int = Int(image!.size.width)
-        var height: Int = Int(image!.size.height)
-        
+        let width: Int = Int(image!.size.width)
+        let height: Int = Int(image!.size.height)
         let currentContext = CGContext(data: nil, width: width, height: height, bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
-        
         let layer = CGLayer(currentContext!, size: image!.size, auxiliaryInfo: nil)!
-        
-        layer.context!.draw(CGIMAGEPLACEHOLDER, in: CGRect(origin: .zero, size: image!.size))
-        
+        layer.context!.draw(imageRef!, in: CGRect(origin: .zero, size: image!.size))
         return layer
     }
 }
