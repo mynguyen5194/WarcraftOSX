20b71db30d5217c6a5a21ae3599ffd259876e349
Alan Wei <alanwei@Alans-MacBook-Pro.local>
2017-02-09 00:15:29 -0800

    Update peasant and objects

--- WarcraftOSX/SecondController.swift
+++ WarcraftOSX/SecondController.swift
@@ -19,19 +19,32 @@ class SecondController: NSViewController {
         
         object.viewDidLoad()
         
-        var imageView = NSImageView()
-        var images = NSImage()
-        
-        
         peasantView = NSImageView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
         peasant = NSImage(cgImage: (object.objectDictArray["Peasant"]![0]), size: NSZeroSize)
         peasantView.image = peasant
         view.addSubview(peasantView)
         
-        imageView = NSImageView(frame: NSRect(x: 100, y: 0, width: 100, height: 100))
-        images = NSImage(cgImage: (object.objectDictArray["GoldMine"]![0]), size: NSZeroSize)
-        imageView.image = images
-        view.addSubview(imageView)
+        var xPos = 50
+        var yPos = 150
+        
+        for i in 1 ..< 6 {
+            let imageView = NSImageView(frame: NSRect(x: xPos, y: yPos, width: 100, height: 100))
+            let images = NSImage(cgImage: (object.objectDictArray[object.objectNames[i]]![0]), size: NSZeroSize)
+            imageView.image = images
+            view.addSubview(imageView)
+            xPos += 100
+        }
+        
+        xPos = 0
+        yPos = 250
+        
+        for i in 6 ..< object.objectDictArray.count {
+            let imageView = NSImageView(frame: NSRect(x: xPos, y: yPos, width: 100, height: 100))
+            let images = NSImage(cgImage: (object.objectDictArray[object.objectNames[i]]![2]), size: NSZeroSize)
+            imageView.image = images
+            view.addSubview(imageView)
+            xPos += 100
+        }
         
         let mouseTrackingArea = NSTrackingArea(rect: peasantView.bounds, options: [NSTrackingAreaOptions.mouseEnteredAndExited, NSTrackingAreaOptions.mouseMoved,NSTrackingAreaOptions.activeInActiveApp], owner: self, userInfo: nil)
 
@@ -45,21 +58,21 @@ class SecondController: NSViewController {
         var npeasantView = NSImageView()
         var npeasant = NSImage()
         
-        if (Int(NSEvent.mouseLocation().y) > 150 && Int(NSEvent.mouseLocation().x) < 225) {
+        if (Int(NSEvent.mouseLocation().y) > 250 && Int(NSEvent.mouseLocation().x) < 425) {
             npeasantView = NSImageView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
             npeasant = NSImage(cgImage: (object.objectDictArray["Peasant"]![0]), size: NSZeroSize)
             npeasantView.image = npeasant
             view.replaceSubview(peasantView, with: npeasantView)
             peasantView = npeasantView
         }
-        else if (Int(NSEvent.mouseLocation().x) > 150 && Int(NSEvent.mouseLocation().y) > 225) {
+        else if (Int(NSEvent.mouseLocation().x) > 415 && Int(NSEvent.mouseLocation().y) > 275) {
             npeasantView = NSImageView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
             npeasant = NSImage(cgImage: (object.objectDictArray["Peasant"]![5]), size: NSZeroSize)
             npeasantView.image = npeasant
             view.replaceSubview(peasantView, with: npeasantView)
             peasantView = npeasantView
         }
-        else if (Int(NSEvent.mouseLocation().y) < 150 && Int(NSEvent.mouseLocation().x) > 225) {
+        else if (Int(NSEvent.mouseLocation().y) < 350 && Int(NSEvent.mouseLocation().x) > 425) {
             npeasantView = NSImageView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
             npeasant = NSImage(cgImage: (object.objectDictArray["Peasant"]![10]), size: NSZeroSize)
             npeasantView.image = npeasant
