1becd4acd279bebcd18c0c6313e9464f89be0caa
mynguyen5194 <mydng@ucdavis.edu>
2017-02-13 13:11:34 -0800

    Adding menu

---
+++ WarcraftOSX/SplashScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashScreenViewController.swift
+//  WarcraftOSX
+//
+//  Created by My Nguyen on 2/12/17.
+//  Copyright © 2017 Michelle Lee. All rights reserved.
+//
+
+import Cocoa
+import AVFoundation
+
+class SplashScreenViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
