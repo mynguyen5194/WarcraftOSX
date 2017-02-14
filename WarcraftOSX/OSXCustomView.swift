53a4f896e1df41fd523792cc0c71f60ed9aa551d
giheok <giheok.nam@gmail.com>
2017-02-14 13:21:52 -0800

    Implement mouse dragging

--- WarcraftOSX/OSXCustomView.swift
+++ WarcraftOSX/OSXCustomView.swift
@@ -31,18 +31,6 @@ class OSXCustomView: NSView {
         frame.origin.y -= event.deltaY
     }
     
-    //NOTE: Look into NSScrollView
-    /*
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let location = touches.first?.location(in: self), let previousLocation = touches.first?.previousLocation(in: self) else {
-            return
-        }
-        frame.origin.x += location.x - previousLocation.x
-        frame.origin.y += location.y - previousLocation.y
-        frame.origin.x = max(min(frame.origin.x, 0), -frame.size.width + UIScreen.main.bounds.width)
-        frame.origin.y = max(min(frame.origin.y, 0), -frame.size.height + UIScreen.main.bounds.height)
-    }*/
-  
     override func draw(_ dirtyRect: CGRect) {
         
         guard let mapRenderer = mapRenderer, let assetRenderer = assetRenderer else {
@@ -56,7 +44,6 @@ class OSXCustomView: NSView {
             try assetRenderer.drawAssets(on: layer, typeSurface: typeLayer, in: rectangle)
             try mapRenderer.drawMap(on: layer, typeSurface: typeLayer, in: rectangle, level: 1)
             let context = UIGraphicsGetCurrentContext()!
-            let mainRect = CGRect(origin: .zero, size: CGSize(width: 900, height: 600))
             context.draw(layer as! CGLayer, in: self.bounds)
             context.draw(typeLayer as! CGLayer, in: dirtyRect)
         } catch {
